af1232abf7feaaf148ed8be3e116c6a37590c9f8
Tatyana <nikolina.tatyana@yandex.ru>
2023-08-03 20:59:22 +0500

    добавила верстку на страницы

--- project-messenger/vite.config.js
+++ project-messenger/vite.config.js
@@ -3,6 +3,10 @@ import { defineConfig } from 'vite'
 import handlebars from 'vite-plugin-handlebars';
 
 export default defineConfig({
+    root: resolve(__dirname, 'src'),
+    build: {
+        outDir: resolve(__dirname, 'dist')
+    },
     plugins: [handlebars({
         partialDirectory: resolve(__dirname, 'src/partials'),
         context: {
