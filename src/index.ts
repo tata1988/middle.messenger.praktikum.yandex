800087a8e797934d5b1dee7d189164d1bfc74db2
Tatyana <nikolina.tatyana@yandex.ru>
2023-08-25 15:31:27 +0500

    +модалка в меню Chat

--- src/index.ts
+++ src/index.ts
@@ -10,17 +10,10 @@ import { Label } from './components/label';
 import { Line } from './components/line';
 import { ChatItem } from './blocks/chatList/chatItem';
 import { ChatList } from './blocks/chatList';
-import { WrapperMenu } from './components/wrapperMenu';
-import { Menu } from './blocks/menu';
 import { MessagesHeader } from './blocks/messageFeed/messagesHeader';
 import { MessagesFooter } from './blocks/messageFeed/messagesFooter';
-import { DataChange } from './blocks/dataChange';
-import { PasswordChange } from './blocks/passwordChange';
 import { AvatarChange } from './blocks/avatarChange';
-import { ModalAddUser } from './blocks/modal/modalAddUser';
-import { ModalDeleteUser } from './blocks/modal/modalDeleteUser';
 import { Error } from './components/error';
-
 
 registerComponent('Button', Button);
 registerComponent('ButtonSubmit', Button);
@@ -31,16 +24,9 @@ registerComponent('Line', Line);
 registerComponent('Error', Error);
 registerComponent('ChatItem', ChatItem);
 registerComponent('ChatList', ChatList);
-registerComponent('wrapperMenu', WrapperMenu);
-registerComponent('Menu', Menu);
-registerComponent('ModalAddUser', ModalAddUser);
-registerComponent('ModalDeleteUser', ModalDeleteUser);
 registerComponent('MessagesHeader', MessagesHeader);
 registerComponent('MessagesFooter', MessagesFooter);
 registerComponent('AvatarChange', AvatarChange);
-registerComponent('DataChange', DataChange);
-registerComponent('PasswordChange', PasswordChange);
-
 
 window.addEventListener('DOMContentLoaded', () => {
    render('auth')
--- src/pages/profile/index.ts
+++ src/pages/profile/index.ts
@@ -19,18 +19,99 @@ export class Profile extends Block {
         email: 'email',
         phone: 'phone',
       },
+      title: {
+        login: 'от 3 до 20 символов, латиница, может содержать цифры, но не состоять из них, без пробелов, без спецсимволов (допустимы дефис и нижнее подчёркивание).',
+        password: 'от 8 до 40 символов, обязательно хотя бы одна заглавная буква и цифра.',
+        name: 'Латиница или кириллица, первая буква должна быть заглавной, без пробелов и без цифр, нет спецсимволов (допустим только дефис).',
+        email: 'Латиница, может включать цифры и спецсимволы вроде дефиса и подчёркивания, обязательно должна быть «собака» (@) и точка после неё, но перед точкой обязательно должны быть буквы.',
+        tel: 'от 10 до 15 символов, состоит из цифр, может начинается с плюса.',
+      },
       edit: false,
       isData: true,
       btnState: true,
+
       changeData: () => {
         this.setProps({edit: true, isData: true})
         },
       changePassword: () => {
       this.setProps({edit: true, isData: false, btnState: false})
       },
+      
+      blurEmail: (e: Event) => {
+        e.preventDefault(); 
+        const email = this.refs.email;
+        const emailValue = (email as Input).value();
+        if (!validation(email, emailValue)) {
+          console.log('Пожалуйста, правильно заполните поле');
+        }
+      },
+      blurLogin: (e: Event) => {
+        e.preventDefault();
+        const login = this.refs.login;
+        const loginValue = (login as Input).value();
+        if (!validation(login, loginValue)) {
+          console.log('Пожалуйста, правильно заполните поле');
+        }     
+      },
+      blurFirstName: (e: Event) => {
+        e.preventDefault();
+        const firstName = this.refs.first_name;
+        const firstNameValue = (firstName as Input).value(); 
+        if (!validation(firstName, firstNameValue)) {
+          console.log('Пожалуйста, правильно заполните поле');
+        }    
+      },
+      blurSecondName: (e: Event) => {
+        e.preventDefault();
+        const secondName = this.refs.second_name;
+        const secondNameValue = (secondName as Input).value();
+        if (!validation(secondName, secondNameValue)) {
+          console.log('Пожалуйста, правильно заполните поле');
+        }      
+      },
+      blurDisplayName: (e: Event) => {
+        e.preventDefault();
+        const displayName = this.refs.display_name;
+        const displayNameValue = (displayName as Input).value();
+        if (!validation(displayName, displayNameValue)) {
+          console.log('Пожалуйста, правильно заполните поле');
+        }      
+      },
+      blurTel: (e: Event) => {
+        e.preventDefault();
+        const phone = this.refs.phone;
+        const phoneValue = (phone as Input).value();  
+        if (!validation(phone, phoneValue)) {
+          console.log('Пожалуйста, правильно заполните поле');
+        }   
+      },
+      blurOldPassword: (e: Event) => {
+          e.preventDefault();  
+          const oldPassword = this.refs.oldPassword;
+          const oldPasswordValue = (oldPassword as Input).value();
+          if (!validation(oldPassword, oldPasswordValue)) {
+            console.log('Пожалуйста, правильно заполните поле');
+          } 
+               
+      },
+      blurNewPassword: (e: Event) => {
+          e.preventDefault();    
+          const newPassword = this.refs.newPassword;
+          const newPasswordValue = (newPassword as Input).value(); 
+          if (!validation(newPassword, newPasswordValue)) {
+            console.log('Пожалуйста, правильно заполните поле');
+          }
+      },
+      blurNewPasswordRepeat: (e: Event) => {
+        e.preventDefault();    
+        const newPasswordRepeat = this.refs.newPasswordRepeat;
+        const newPasswordRepeatValue = (newPasswordRepeat as Input).value(); 
+        if (!validation(newPasswordRepeat, newPasswordRepeatValue)) {
+          console.log('Пожалуйста, правильно заполните поле');
+        }
+    },
       onClickData: (e: Event) => {
         e.preventDefault();
-        console.log('onClickData');
         
         const login = this.refs.login;
         const email = this.refs.email;
@@ -59,6 +140,7 @@ export class Profile extends Block {
                 secondName: secondNameValue,
                 phone: phoneValue
             });
+            this.setProps({edit: false, isData: true});
         } else {
             alert('Пожалуйста, правильно заполните все поля');
             return;
@@ -66,8 +148,7 @@ export class Profile extends Block {
       },
       onClickPassword: (e: Event) => {
         e.preventDefault();
-        console.log('onClickPassword');
-        
+          
         const oldPassword = this.refs.oldPassword;
         const newPassword = this.refs.newPassword;
         const newPasswordRepeat = this.refs.newPasswordRepeat;
@@ -78,10 +159,11 @@ export class Profile extends Block {
         if (validation(oldPassword, oldPasswordValue) &&
             validation(newPassword, newPasswordValue) &&
             validation(newPasswordRepeat, newPasswordRepeatValue) &&
-            newPasswordValue == newPasswordRepeatValue) {
+            newPasswordValue === newPasswordRepeatValue) {
             console.log({
               password: newPasswordValue,
             });
+            this.setProps({edit: false, isData: true});
         } else {
           console.log('oldPasswordValue', oldPasswordValue, 'newPassword', newPasswordValue, 'newPasswordRepeatValue', newPasswordRepeatValue);
           alert('Пожалуйста, правильно заполните все поля');
