f49ef1be4a0ff07d9f13406aabdcc35048cca78f
Tatyana <nikolina.tatyana@yandex.ru>
2023-09-11 22:44:23 +0500

    добавила "express-history-api-fallback"

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
+import fallback from "express-history-api-fallback";
 import express from "express";
 
 const app = express();
+const root = `${__dirname}/dist`;
 const PORT = 3000;
 
-app.use(express.static("./dist"));
+app.use(express.static(root));
+app.use(fallback("index.html", { root }));
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
