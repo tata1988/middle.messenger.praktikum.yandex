c09688f708ba60836be3e01ccdebda99b8d37263
Tatyana <nikolina.tatyana@yandex.ru>
2023-09-27 21:13:54 +0500

    тесты для Block,настройка precommit

--- src/utils/HttpTransport.test.ts
+++ src/utils/HttpTransport.test.ts
@@ -1,28 +1,38 @@
-import { SinonStub, createSandbox } from 'sinon';
-import { expect, use } from 'chai';
-import sinonChai from 'sinon-chai';
-import HTTPTransport from './HttpTransport.ts';
+import sinon, {
+  SinonFakeXMLHttpRequest,
+  SinonFakeXMLHttpRequestStatic,
+} from "sinon";
+import { expect } from "chai";
 
-describe('HttpTransport', () => {
-  use(sinonChai);
-  const sandbox = createSandbox();
-  let http: HTTPTransport;
-  let request: SinonStub<any>;
+import HTTPTransport from "./HttpTransport.ts";
+
+describe("HTTPTransport", () => {
+  let xhr: SinonFakeXMLHttpRequestStatic;
+  let instance: HTTPTransport;
+  let requests: SinonFakeXMLHttpRequest[] = [];
 
   beforeEach(() => {
-    http = new HTTPTransport('/');
-    request = sandbox.stub(http, 'request' as keyof typeof http).callsFake(() => Promise.resolve());
+    xhr = sinon.useFakeXMLHttpRequest();
+
+    // @ts-ignore
+    global.XMLHttpRequest = xhr;
+
+    xhr.onCreate = (request: SinonFakeXMLHttpRequest) => {
+      requests.push(request);
+    };
+
+    instance = new HTTPTransport("/auth");
   });
 
   afterEach(() => {
-    sandbox.restore();
+    requests = [];
   });
 
-  it('преобразовывает объект запроса в строку для запроса GET, где все параметры являются строками', () => {
+  it(".get() should send GET request", () => {
+    instance.get("/user");
 
-    http.get("", {data: {a: 1, b: 2}});
+    const [request] = requests;
 
-    expect(request).calledWithMatch("?a=1&b=2", "GET");
+    expect(request.method).to.eq("GET");
   });
-
 });
